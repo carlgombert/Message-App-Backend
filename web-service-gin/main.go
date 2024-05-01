b7dff3ac3707f10d5f76d44a660dd9d4641eb850
Carl Gombert <carlgombert2@gmail.com>
2024-05-01 13:50:11 -0500

    setting up database

--- web-service-gin/main.go
+++ web-service-gin/main.go
@@ -1,10 +1,19 @@
 package main
 
 import (
+	"context"
+	"fmt"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
+
+const uri = "mongodb://127.0.0.1:27017/messageApp"
 
 type test struct {
 	ID    string `json:"id"`
@@ -17,6 +26,31 @@ var testArr = []test{
 }
 
 func main() {
+	//connect to db
+	// Use the SetServerAPIOptions() method to set the Stable API version to 1
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+	opts := options.Client().ApplyURI(uri).SetServerAPIOptions(serverAPI)
+
+	// Create a new client and connect to the server
+	client, err := mongo.Connect(context.TODO(), opts)
+
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		if err = client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+
+	// Send a ping to confirm a successful connection
+	var result bson.M
+	if err := client.Database("admin").RunCommand(context.TODO(), bson.D{{"ping", 1}}).Decode(&result); err != nil {
+		panic(err)
+	}
+	fmt.Println("Pinged your deployment. You successfully connected to MongoDB!")
+
+	// configure apis
 	router := gin.Default()
 
 	//test APIs
