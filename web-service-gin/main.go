bc84cae2eb12639a466170b8bd094dc8d1942ede
Carl Gombert <carlgombert2@gmail.com>
2024-05-01 13:11:33 -0500

    testing golang apis

---
+++ web-service-gin/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type test struct {
+	ID    string `json:"id"`
+	Title string `json:"title"`
+}
+
+var testArr = []test{
+	{ID: "1", Title: "Blue Train"},
+	{ID: "2", Title: "Jeru"},
+}
+
+func main() {
+	router := gin.Default()
+
+	//test api
+	router.GET("/test", getTest)
+
+	router.Run("localhost:8080")
+}
+
+func getTest(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, testArr)
+}
